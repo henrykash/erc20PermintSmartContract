943b05fc23d52e5f186185a583634f334f27d072
henrykash <henrykariuki003@gmail.com>
2023-06-21 20:20:03 +0300

    erc20-permit logi

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,35 @@
+import { signERC2612Permit } from "eth-permit";
+import { ethers } from "ethers";
+import { config } from "./config/config";
+import { TokenABI } from "./constants/constants";
 
-import { signERC2612Permit } from 'eth-permit';
-import { ethers } from 'ethers';
-const value = ethers.utils.parseUnits('10', 18);
+const main = async () => {
+  const wallet = new ethers.Wallet(config.PRIVATE_KEY);
+  const senderAddress = await wallet.getAddress();
+ const _value: any = ethers.utils.parseUnits("10", 18)
 
-// const wallet = new ethers.Wallet(, new ethers.providers.JsonRpcProvider(rpcUrl));
-// const senderAddress = await wallet.getAddress();
+  const result = await signERC2612Permit(
+    wallet,
+    config.token,
+    senderAddress,
+    config.spender,
+    _value
+  );
 
-// const result = await signERC2612Permit(wallet, tokenAddress, senderAddress, spender, value);
+  const tokenContract = new ethers.Contract(config.token,TokenABI , wallet);
 
-// await token.methods.permit(senderAddress, spender, value, result.deadline, result.v, result.r, result.s).send({
-//   from: senderAddress,
-// });
+
+  await tokenContract.methods
+    .permit(
+      senderAddress,
+      config.spender,
+      config.value,
+      result.deadline,
+      result.v,
+      result.r,
+      result.s
+    )
+    .send({
+      from: senderAddress,
+    });
+};
